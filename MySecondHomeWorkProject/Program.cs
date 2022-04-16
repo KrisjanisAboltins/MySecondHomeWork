1ff645f1ba47446e8a5ee54c0d2e7b7d938ab0cc
KrisjanisAboltins <krisjanis.aboltins@inbox.lv>
2022-04-16 11:18:25 +0300

    1 korekcija

---
+++ MySecondHomeWorkProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+string greeting = "labdien, mans vārds ir Krišjānis un Jūs skatāties manu otro mājasdarbu";
+Console.WriteLine(greeting);
